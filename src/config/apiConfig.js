03f83c5e861c79afc49b8d57fa3d78f37103e864
ojiemekeme armstrong <ojieprincewill24@gmail.com>
2026-05-13 09:43:47 +0100

    updated api config file

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,6 +1,3 @@
-const rawApiBaseUrl = import.meta.env.PROD
-  ? "https://hello-dreams-ai.onrender.com"
-  : import.meta.env?.VITE_API_BASE_URL ||
-    "https://hello-dreams-ai.onrender.com";
+const rawApiBaseUrl = "https://hello-dreams-ai.onrender.com";
 
 export const API_BASE_URL = rawApiBaseUrl.replace(/\/+$/, "");
